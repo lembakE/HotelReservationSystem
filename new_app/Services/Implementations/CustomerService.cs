b68e152869eb66957e94fef53fc6cd075d7d1a5e
LembakV <volodymyr_lembak@epam.com>
2025-08-26 12:29:10 +0000

    Add CustomerService implementation for SOAP services

---
+++ new_app/Services/Implementations/CustomerService.cs
@@ -0,0 +1,58 @@
+using HotelReservationSystem.Data;
+using HotelReservationSystem.Models.Domain;
+using HotelReservationSystem.Services.Contracts;
+
+namespace HotelReservationSystem.Services.Implementations
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public Customer? GetCustomer(int id)
+        {
+            return _context.Customers.SingleOrDefault(c => c.Id == id);
+        }
+
+        public int CreateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer.Id;
+        }
+
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new Exception($"Customer with ID {id} not found");
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+
+            _context.SaveChanges();
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                throw new Exception($"Customer with ID {id} not found");
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+        }
+    }
+}
