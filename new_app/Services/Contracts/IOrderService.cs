484bb9876ce5399d8a34f6c4f95e2eb3e797aa55
LembakV <volodymyr_lembak@epam.com>
2025-08-26 12:40:47 +0000

    Update IOrderService to include async methods

--- new_app/Services/Contracts/IOrderService.cs
+++ new_app/Services/Contracts/IOrderService.cs
@@ -1,8 +1,19 @@
-using HotelReservationSystem.Models.Domain;
-using HotelReservationSystem.Models.DTOs;
-using System.ServiceModel;
+aproLambinator result: 
 
-namespace HotelReservationSystem.Services.Contracts
+using System.Collections.Generic;
+using HotelRetelingSivation.HotelManag: IHcontrat:  ssing
+using HotelReservation.Models.Models.Domain;
+using HotelReselation.Models.Models.Models;
+using HOTEL.Models.ToDTOs.emsk;
+using System.Collections.Generic:
+using System;
+using System.ServiceModel;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace HAssemtReservation.Services.Contracts
+{
+using System.ServiceM
 {
     [ServiceContract]
     public interface IOrderService
@@ -21,5 +32,16 @@ namespace HotelReservationSystem.Services.Contracts
 
         [OperationContract]
         void DeleteOrder(int id);
+        
+        // Async methods (not exposed as SOAP operations)
+        Task<IEnumerable<Order>> GetOrdersAsync();
+        
+        Task<Order?> GetOrderAsync(int id);
+        
+        Task<int> CreateNewOrderAsync(NewOrderDto newOrder);
+        
+        Task UpdateOrderAsync(int id, Order order);
+        
+        Task DeleteOrderAsync(int id);
     }
 }
